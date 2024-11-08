094e0405f6ffc28fcc1faa62b07933d5843c8f73
Shishir-vardhan <shishirvardhan695@gmail.com>
2024-11-08 13:12:33 +0530

    .Net8 Razor project

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(30)]
+		public string Name { get; set; }
+
+		[Range(0, 1000)]
+		public int DisplayOrder { get; set; }
+	}
+}
